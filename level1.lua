a3661ed03d99650dcaac95e9434be9d4d654c2f8
Lars Eliassen <lars.eliassen@bekk.no>
2013-08-24 13:51:45 +0200

    Noe som funker sånn tålelig.

--- level1.lua
+++ level1.lua
@@ -13,14 +13,54 @@ physics.start(); physics.pause()
 
 local widget = require "widget"
 
-local NUMBER_OF_CRATES = 15
+local NUMBER_OF_CRATES = 20
 local ZOOM_FACTOR = 0.2
+
+highscore = 0
+
+
 --------------------------------------------
 
 -- forward declarations and other locals
 local screenW, screenH, halfW = display.contentWidth, display.contentHeight, display.contentWidth*0.5
 
+local background1 = display.newImage( "background.png")
+background1:setReferencePoint( display.BottomLeftReferencePoint )
+background1.x, background1.y = display.contentWidth, 75
+background1.speed = 0.25
+
+local background1_2 = display.newImage( "background.png")
+background1_2:setReferencePoint( display.BottomLeftReferencePoint )
+background1_2.x, background1.y = display.contentWidth*2, 75
+background1_2.speed = 0.25
+
+local background2 = display.newImage( "background2.png")
+background2:setReferencePoint( display.BottomLeftReferencePoint )
+background2.x, background2.y = display.contentWidth*2, 200
+background2.speed = 0.5
+
+local background2_2 = display.newImage( "background2.png")
+background2_2:setReferencePoint( display.BottomLeftReferencePoint )
+background2_2.x, background2_2.y = display.contentWidth, 200
+background2_2.speed = 0.5
+
+local background3 = display.newImage( "background3.png")
+background3:setReferencePoint( display.BottomLeftReferencePoint )
+background3.x, background3.y = -display.contentWidth*2, 150
+background3.speed = -0.5
+
+local background3_2 = display.newImage( "background3.png")
+background3_2:setReferencePoint( display.BottomLeftReferencePoint )
+background3_2.x, background3_2.y = -display.contentWidth, 150
+background3_2.speed = -0.5
+
+local bg_low = display.newImage ("bg_low2.png")
+bg_low:setReferencePoint( display.BottomLeftReferencePoint)
+bg_low.x, bg_low.y = 0, display.contentHeight
+--skies:insert( bg_low)
+
 local countedDown = false
+local finished = false
 
 local function onPlayBtnRelease()
 	
@@ -51,6 +91,8 @@ local function restart()
 	displayWin.text = ""
 	displayTime.text = ""
 	countedDown = false
+	finished = false
+	physics.setGravity( 0, 10 )
 end
 
 -----------------------------------------------------------------------------------------
@@ -62,20 +104,14 @@ end
 -----------------------------------------------------------------------------------------
 function onScreenTouched(event)
 	if event.phase == "began" and numberOfCrates < NUMBER_OF_CRATES then
-		--group:removeChild(crate)
 		print("touch")
-		local size = math.random(10,30)
+		local size = math.random(20,30)
 		crate = display.newImageRect( "crate.png", size, size )	
-		crate.x, crate.y = event.x, event.y	
-		--group:scale(1/ZOOM_FACTOR,1/ZOOM_FACTOR)
-		--crates:scale(1/ZOOM_FACTOR, 1/ZOOM_FACTOR)
-		--for i=crates.numChildren,1,-1 do
-        --	local child = crates[i]
-        --	child:scale(1/ZOOM_FACTOR, 1/ZOOM_FACTOR)
-		--end
-		--crate:scale(1/ZOOM_FACTOR, 1/ZOOM_FACTOR)	
+		crate.x, crate.y = event.x - 50, event.y + 50		
 	elseif event.phase == "moved" and numberOfCrates < NUMBER_OF_CRATES then
-		crate.x, crate.y = event.x, event.y	
+		if crate ~= nil then
+			crate.x, crate.y = event.x - 50, event.y	+ 50
+		end
 	elseif event.phase == "ended" and numberOfCrates < NUMBER_OF_CRATES then
 		print("ended")
 		print(crates.numChildren)
@@ -87,17 +123,7 @@ function onScreenTouched(event)
 			numberOfCrates = numberOfCrates +1
 		end
 		print(table.getn(crateTable))
-		--group:scale(ZOOM_FACTOR,ZOOM_FACTOR)
-		--crates:scale(ZOOM_FACTOR,ZOOM_FACTOR)
-		--for i=crates.numChildren,1,-1 do
-        --	local child = crates[i]
-        --	child:scale(ZOOM_FACTOR, ZOOM_FACTOR)
-		--end
-		--crate:scale(ZOOM_FACTOR, ZOOM_FACTOR)
-	--elseif(numberOfCrates == NUMBER_OF_CRATES) and not countedDown then
-	--	countdown(group, crateTable)
-	elseif countedDown then
-	 	--storyboard.gotoScene( "dummyScene", "fade", 500 )
+	elseif countedDown == true then
 	 	restart()			
 	end	
 end
@@ -105,10 +131,11 @@ end
 Runtime:addEventListener("touch", onScreenTouched)
 
 function frameListener(event)
-	if (numberOfCrates == NUMBER_OF_CRATES) and not countedDown then
+	if (numberOfCrates == NUMBER_OF_CRATES) and finished == false then
+		finished = true
 		countdown()
-		countedDown = true
 	end
+	updateBackgrounds()
 end
 
 Runtime:addEventListener("enterFrame", frameListener)
@@ -119,7 +146,6 @@ Runtime:addEventListener("enterFrame", frameListener)
 
 function countdown() 
 	local levelTime = 5
-	--displayTime.text = levelTime
 	local function checkTime(event)
 		
 	  	levelTime = levelTime - 1
@@ -127,7 +153,6 @@ function countdown()
 	  			
 	  	if ( levelTime <= 0 ) then
 	  		maxCrate = 5000
-	  		--if( numberOfCrates < 1000 ) then
 	  			print("CRATES:", table.getn(crateTable))
 		  		for i=0,numberOfCrates-1,1 do
 		  			print(crateTable[i].y)
@@ -137,24 +162,75 @@ function countdown()
 		  			end
 		  			print("maxcrate:", maxCrate)
 		  		end
-		  	--end
 		  	print("ScreenH", screenH, "maxcrate:", maxCrate)
-	  		displayTime.text = screenH - math.floor(maxCrate)
+		  	score = screenH - math.floor(maxCrate)
+	  		displayTime.text = score
+	  		print("SCORE", score)
+	  		
 	  		if maxCrate < 250 then
 	  			displayWin.text = "YOU WIN"
 	  			countedDown = true
+	  			print("SCORE 1", score)
+	  			if(score > highscore) then
+	  				highscore = score		
+	  				displayHS.text = highscore
+	  			end
+	  			crate = display.newImageRect( "crate.png", 100, 100 )	
+				crate.x, crate.y = 160, -100
+				physics.addBody( crate, { density=10.5, friction=0.3, bounce=0.0 } )
+				crates:insert (crate)
+				physics.setGravity( 5, 10 )
 	  	    else
 	  	    	displayWin.text = "YOU LOOSE"
 	  	    	countedDown = true
+	  	    	print("SCORE 2", score, "HS", highscore)
+	  	    	if(score > highscore) then
+	  				highscore = score
+	  				displayHS.text = highscore
+	  			end
+	  			crate = display.newImageRect( "crate.png", 100, 100 )	
+				crate.x, crate.y = 160, -100
+				physics.addBody( crate, { density=10.5, friction=0.3, bounce=0.0 } )
+				crates:insert (crate)
+				physics.setGravity( 5, 10 )
 	  	    end
+	  	    countedDown = true
 	  	end
 	end
 	timer.performWithDelay( 1000, checkTime, levelTime )
+
 end
 
-function scene:createScene( event )
-	print("create scene")
-	scene:addEventListener( "enterScene", scene )
+function scene:createScene( event )	
+end
+
+function updateBackgrounds()
+	background1.x = background1.x - background1.speed*0.5
+	if(background1.x < -display.contentWidth) then
+		background1.x = display.contentWidth
+	end
+	background1_2.x = background1_2.x - background1_2.speed*0.5
+	if(background1_2.x < -display.contentWidth) then
+		background1_2.x = display.contentWidth
+	end
+
+	background2.x = background2.x - background2.speed*0,5
+	if(background2.x < -display.contentWidth) then
+		background2.x = display.contentWidth
+	end
+	background2_2.x = background2_2.x - background2_2.speed*0.5
+	if(background2_2.x < -display.contentWidth) then
+		background2_2.x = display.contentWidth
+	end
+
+	background3.x = background3.x - background3.speed*0.5
+	if(background3.x > display.contentWidth) then
+		background3.x = -display.contentWidth
+	end
+	background3_2.x = background3_2.x - background3_2.speed*0.5
+	if(background3_2.x > display.contentWidth) then
+		background3_2.x = -display.contentWidth
+	end
 end
 
 function scene:enterScene( event )
@@ -162,26 +238,29 @@ function scene:enterScene( event )
 	group = self.view
 	numberOfCrates = 0	
 	crateTable = {}
-	
-	--local group = self.view
-	local background = display.newRect( 0, 0, screenW, screenH )
-	background:setFillColor( 128 )
+	local background = display.newRect(0,0,screenW, screenH)
+	background:setFillColor( 53, 175, 229, 255 )
+	background:toBack()
 
 	local crate = display.newImageRect( "crate.png", 19, 19 )
 	crate.x, crate.y = 160, -100
 	crate.rotation = 30
 
 	local goalLine = display.newLine( 0, 250, 320, 250)
+	goalLine:setColor(255, 255, 255, 150)
 	physics.addBody( crate, { density=1.0, friction=0.3, bounce=1 } )
 
-	local grass = display.newImageRect( "grass.png", screenW, 40 )
+
+	local grass = display.newImageRect("bg_low2.png", screenW, 40 )
+	grass:setFillColor(10, 255, 20, 200)
 	grass:setReferencePoint( display.BottomLeftReferencePoint )
 	grass.x, grass.y = 0, display.contentHeight
 
-	local grassShape = { -halfW,-15, halfW,-15, halfW,15, -halfW,15 }
+	local grassShape = { -halfW,-10, halfW,-10, halfW,15, -halfW,15 }
 	physics.addBody( grass, "static", { friction=0.3, shape=grassShape } )
 
-	group:insert( background )
+	group:insert( background1 )
+	group:insert( background2 )
 	group:insert( grass)
 	group:insert( crate )
 	group:insert( goalLine)
@@ -192,6 +271,8 @@ function scene:enterScene( event )
 	displayCrates = display.newText( "", 10, 0, "Helvetica", 20 )
 	displayWin = display.newText( "", 150, 150, "Helvetica", 50 )
 	displayTime = display.newText( "", 160, 240, "Helvetica", 100 )
+	displayHSStatic = display.newText("Highscore:", 170, 0, "Helvetica", 20)
+	displayHS = display.newText(highscore, 280, 0, "Helvetica", 20)
 	textz:insert(displayTime)
 	textz:insert(displayWin)
 	textz:insert(displayCrates)	
@@ -201,27 +282,9 @@ end
 
 -- Called when scene is about to move offscreen:
 function scene:exitScene( event )
-	print("exitScene")
-	--local group = self.view
-	--display.remove(group)
-	display.remove(textz)
-	display.remove(crates)
-	--crateTable = {}
-	for p,v in pairs( crateTable ) do
-	--	display.remove( crateTable[p] )
-		crateTable[p] = nil
-	end
-	crateTable = nil
-	numberOfCrates = 0
-	physics.pause()
-
-	--Runtime:removeEventListener("touch", onScreenTouched)	
 end
-
 -- If scene's view is removed, scene:destroyScene() will be called just prior to:
 function scene:destroyScene( event )
-	print("destroy!!!")
-
 end
 
 
@@ -235,7 +298,7 @@ print("add listeners")
 scene:addEventListener( "createScene", scene )
 
 -- "enterScene" event is dispatched whenever scene transition has finished
-
+scene:addEventListener( "enterScene", scene )
 
 -- "exitScene" event is dispatched whenever before next scene's transition begins
 scene:addEventListener( "exitScene", scene )
--- menu.lua
+++ menu.lua
@@ -6,9 +6,90 @@
 
 local storyboard = require( "storyboard" )
 local scene = storyboard.newScene()
-
+local skies = display.newGroup()
 -- include Corona's "widget" library
 local widget = require "widget"
+
+local physics = require "physics"
+physics.start(); physics.pause()
+
+	
+local background = display.newRect( 0, 0, display.contentWidth, display.contentHeight)
+background:setFillColor( 53, 175, 229, 255 )
+background:toBack()
+--local background = display.newImageRect( "background.jpg", display.contentWidth, display.contentHeight )
+background:setReferencePoint( display.TopLeftReferencePoint )
+background.x, background.y = 0, 0
+--skies:toBack()
+
+local bg_low = display.newImage ("bg_low.png")
+bg_low:setReferencePoint( display.BottomLeftReferencePoint)
+bg_low.x, bg_low.y = 0, display.contentHeight
+skies:insert( bg_low)
+
+	
+
+local background1 = display.newImage( "background.png")
+background1:setReferencePoint( display.BottomLeftReferencePoint )
+background1.x, background1.y = display.contentWidth, 75
+background1.speed = 0.5
+skies:insert( background1 )
+--background1:toBack()
+
+
+local background1_2 = display.newImage( "background.png")
+background1_2:setReferencePoint( display.BottomLeftReferencePoint )
+background1_2.x, background1.y = display.contentWidth*2, 75
+background1_2.speed = 0.5
+--background1_2:toBack()
+skies:insert( background1_2 )
+--background1_2:toBack()
+
+local background2 = display.newImage( "background2.png")
+background2:setReferencePoint( display.BottomLeftReferencePoint )
+background2.x, background2.y = display.contentWidth*2, 420
+background2.speed = 1
+skies:insert( background2 )
+---background2:toBack()
+
+local background2_2 = display.newImage( "background2.png")
+background2_2:setReferencePoint( display.BottomLeftReferencePoint )
+background2_2.x, background2_2.y = display.contentWidth, 420
+background2_2.speed = 1
+skies:insert( background2_2 )
+--background2_2:toBack()
+
+local background3 = display.newImage( "background3.png")
+background3:setReferencePoint( display.BottomLeftReferencePoint )
+background3.x, background3.y = -display.contentWidth*2, 150
+background3.speed = -1
+skies:insert( background3 )
+--background3:toBack()
+
+local background3_2 = display.newImage( "background3.png")
+background3_2:setReferencePoint( display.BottomLeftReferencePoint )
+background3_2.x, background3_2.y = -display.contentWidth, 150
+background3_2.speed = -1
+skies:insert( background3_2 )
+--background3_2:toBack()
+
+local titleLogo = display.newImageRect( "logo.png", 229, 84)
+titleLogo:setReferencePoint( display.CenterReferencePoint )
+titleLogo.x = display.contentWidth * 0.5
+titleLogo.y = 200
+
+	crate = display.newImageRect( "crate.png", 100, 100 )	
+			crate.x, crate.y = 160, -100
+			physics.addBody( crate, { density=10.5, friction=0.3, bounce=0.0 } )
+			skies:insert (crate)
+			crate:toFront()
+			physics.start()
+
+	local grassShape = { -160,-10, 160,-10, 160,15, -160,15 }
+	physics.addBody( bg_low, "static", { friction=0.3, shape=grassShape } )
+
+--skies:insert(titleLogo)
+--titleLogo:toFront()
 
 --------------------------------------------
 
@@ -19,6 +100,10 @@ local playBtn
 local function onPlayBtnRelease()
 	
 	-- go to level1.lua scene
+	for i=skies.numChildren,1,-1 do
+        local child = skies[i]
+        child.parent:remove( child )
+	end
 	storyboard.gotoScene( "level1", "fade", 500 )
 	
 	return true	-- indicates successful touch
@@ -36,31 +121,27 @@ end
 function scene:createScene( event )
 	local group = self.view
 
+
+
 	-- display a background image
-	local background = display.newRect( 0, 0, display.contentWidth, display.contentHeight)
-	background:setFillColor( 128 )
-	--local background = display.newImageRect( "background.jpg", display.contentWidth, display.contentHeight )
-	background:setReferencePoint( display.TopLeftReferencePoint )
-	background.x, background.y = 0, 0
+
 	
 	-- create/position logo/title image on upper-half of the screen
-	local titleLogo = display.newImageRect( "logo.png", 264, 42 )
-	titleLogo:setReferencePoint( display.CenterReferencePoint )
-	titleLogo.x = display.contentWidth * 0.5
-	titleLogo.y = 100
+
 	
-	-- create a widget button (which will loads level1.lua on release)
+	--create a widget button (which will loads level1.lua on release)
 	playBtn = widget.newButton{
-		label="Go Crating",
-		labelColor = { default={255}, over={128} },
-		defaultFile="button.png",
-		overFile="button-over.png",
+		label="GO CRATING ",
+		labelColor = { default={241,55,40,255}, over={128} },
+		--defaultFile="button.png",
+		--overFile="button-over.png",
 		width=154, height=40,
 		onRelease = onPlayBtnRelease	-- event listener function
 	}
 	playBtn:setReferencePoint( display.CenterReferencePoint )
 	playBtn.x = display.contentWidth*0.5
-	playBtn.y = display.contentHeight - 125
+	playBtn.y = display.contentHeight - 180
+	
 	
 	-- all display objects must be inserted into group
 	group:insert( background )
@@ -71,6 +152,7 @@ end
 -- Called immediately after scene has moved onscreen:
 function scene:enterScene( event )
 	local group = self.view
+
 	
 	-- INSERT code here (e.g. start timers, load audio, start listeners, etc.)
 	
@@ -93,6 +175,45 @@ function scene:destroyScene( event )
 		playBtn = nil
 	end
 end
+
+function updateBackgrounds()
+	background1.x = background1.x - background1.speed*0.5
+	if(background1.x < -display.contentWidth) then
+		background1.x = display.contentWidth
+	end
+	--background1:toBack()
+	background1_2.x = background1_2.x - background1_2.speed*0.5
+	if(background1_2.x < -display.contentWidth) then
+		background1_2.x = display.contentWidth
+	end
+	--background1_2:toBack()
+	background2.x = background2.x - background2.speed*0,5
+	if(background2.x < -display.contentWidth) then
+		background2.x = display.contentWidth
+	end
+	background2_2.x = background2_2.x - background2_2.speed*0.5
+	if(background2_2.x < -display.contentWidth) then
+		background2_2.x = display.contentWidth
+	end
+	--background2_2:toBack()
+	background3.x = background3.x - background3.speed*0.5
+	if(background3.x > display.contentWidth) then
+		background3.x = -display.contentWidth
+	end
+	--background3:toBack()
+	background3_2.x = background3_2.x - background3_2.speed*0.5
+	if(background3_2.x > display.contentWidth) then
+		background3_2.x = -display.contentWidth
+	end
+	--background3_2:toBack()
+	titleLogo:toFront()
+end
+
+function frameListener(event)
+	updateBackgrounds()
+end
+
+Runtime:addEventListener("enterFrame", frameListener)
 
 -----------------------------------------------------------------------------------------
 -- END OF YOUR IMPLEMENTATION
